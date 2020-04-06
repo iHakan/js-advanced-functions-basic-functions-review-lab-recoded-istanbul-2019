b0421c0e96fac169a05ecf42817090a7b887b62b
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 21:31:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function saturdayFun(dots="roller-skate" ){
   return `This Saturday, I want to ${dots}!`;
 }
 
-let mondayWork = function(activity){
+let mondayWork = function(activity="go to the office"){
   return `This Monday, I will ${activity}.`
 }
